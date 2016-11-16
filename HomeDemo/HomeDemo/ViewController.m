6b3a98b46de4fa1ef81b76cd2ef9cbd94f8d1a25
dn210 <wsjnh@aliyun.com>
2016-11-16 13:10:37 +0800

    1-初始化

---
+++ HomeDemo/HomeDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  HomeDemo
+//
+//  Created by dn210 on 16/11/16.
+//  Copyright © 2016年 dn210. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
